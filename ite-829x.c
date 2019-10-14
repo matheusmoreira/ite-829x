8a7f395cf82a8f2a9bae8ffe86715f78314aab5b
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2019-10-13 23:13:28 -0300

    Add GPLv2 license header to the ite-829x.c file
    This makes it clear that this source file is licensed under the GPLv2.

--- ite-829x.c
+++ ite-829x.c
@@ -1,5 +1,20 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
+/* Copyright © 2019 Matheus Afonso Martins Moreira
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+ * Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
 #define _POSIX_C_SOURCE 200809L
 #include <stdlib.h>
 #include <stdio.h>
